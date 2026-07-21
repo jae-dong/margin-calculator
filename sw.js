5396959c952cec0c6a6912f3ead7a462712f0c37
jae-dong <jazz6123@naver.com>
2026-07-21 10:43:44 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='adp-v5.1.5-draft-poizon-auto-market';
+const C='adp-v5.1.6-draft-poizon-auto-market';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['/','/index.html?v=5.1.5','/manifest.json','/icon-192.png','/icon-512.png'])))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/index.html?v=5.1.5').then(x=>x||caches.match('/index.html')))))});
