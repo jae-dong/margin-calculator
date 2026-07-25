4443b3130fbe577da14c76a61c3bb222128125aa
jae-dong <jazz6123@naver.com>
2026-07-26 08:24:27 +0900

    리셀 PICK v6.4.0 회원번호·회원목록·로그인보안

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='resell-pick-v6.3.1';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['/','/index.html?v=v6.3.1','/manifest.json','/icon-192.png','/icon-512.png'])))});
+const C='resell-pick-v6.4.0';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['/','/index.html?v=v6.4.0','/manifest.json','/icon-192.png','/icon-512.png'])))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))])));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/index.html?v=v6.3.1').then(x=>x||caches.match('/index.html')))))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/index.html?v=v6.4.0').then(x=>x||caches.match('/index.html')))))});
